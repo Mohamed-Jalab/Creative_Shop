4b9e6c5e58b2672f8fb801c34f824ada188b7c7b
MOHAMMED\Acer <mohjalab9@gamil.com>
2023-08-10 18:35:29 +0300

    Connect the project with firebase

--- creative_shop/lib/main.dart
+++ creative_shop/lib/main.dart
@@ -1,10 +1,14 @@
-import 'package:creative_shop/screens/item_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
-import 'sign_up.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
